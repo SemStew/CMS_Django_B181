dcc937734004405491e8b4a5291e4848a83a812a
Michaela Tauchmanová <36930700+tauchmanovaM@users.noreply.github.com>
2018-12-19 13:38:37 +0100

    Update urls.py

--- SemStew/website/urls.py
+++ SemStew/website/urls.py
@@ -8,9 +8,4 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^semstew/', include('semstew.urls')),
-=======
-from django.contrib import admin
-from django.urls import path
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
+]
