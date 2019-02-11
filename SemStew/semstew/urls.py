ffd2c334a80abe6826a774f91e7c48bab5238bd5
smolidav <smolidav@fit.cvut.cz>
2019-02-11 17:31:20 +0100

    Edited data from db

--- SemStew/semstew/urls.py
+++ SemStew/semstew/urls.py
@@ -1,14 +1,15 @@
 from django.conf.urls import url
+from django.urls import path
 from . import views
 
 app_name = 'semstew'
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    path('', views.index, name='index'),
     url(r'^branches/', views.branches, name='branches'),
     url(r'^offer/', views.offer, name='offer'),
     url(r'^delivery/', views.delivery, name='delivery'),
     url(r'^reservation/', views.reservation, name='reservation'),
-    url(r'^aboutus/', views.aboutus, name='aboutus'),
+    path('aboutus/<int:language_id>', views.about_us, name='about_us'),
     url(r'^contact/', views.contact, name='contact'),
 ]
--- SemStew/semstew/views.py
+++ SemStew/semstew/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.template import loader
+from semstew.models import *
+
 
 def index (request):
     template = loader.get_template('../templates/semstew/index.html')
@@ -8,12 +10,14 @@ def index (request):
     }
     return render(request, '../templates/semstew/index.html', context)
 
+
 def branches (request):
     template = loader.get_template('../templates/semstew/branches.html')
     context = {
         # Everything what is need from DB to HTML
     }
     return render(request, '../templates/semstew/branches.html', context)
+
 
 def offer (request):
     template = loader.get_template('../templates/semstew/offer.html')
@@ -22,12 +26,14 @@ def offer (request):
     }
     return render(request, '../templates/semstew/offer.html', context)
 
+
 def delivery (request):
     template = loader.get_template('../templates/semstew/delivery.html')
     context = {
         # Everything what is need from DB to HTML
     }
     return render(request, '../templates/semstew/delivery.html', context)
+
 
 def reservation (request):
     template = loader.get_template('../templates/semstew/reservation.html')
@@ -36,12 +42,16 @@ def reservation (request):
     }
     return render(request, '../templates/semstew/reservation.html', context)
 
-def aboutus (request):
-    template = loader.get_template('../templates/semstew/aboutus.html')
+
+def about_us(request, language_id):
+    about_us_headers = AboutUsConfig.objects.get(language=language_id)
+    about_us_contents = AboutUs.objects.get(aboutus_config=about_us_headers.pk)
     context = {
-        # Everything what is need from DB to HTML
+        'about_us_headers': about_us_headers,
+        'about_us_contents': about_us_contents
     }
-    return render(request, '../templates/semstew/aboutus.html', context)
+    return render(request, 'semstew/aboutus.html', context)
+
 
 def contact (request):
     template = loader.get_template('../templates/semstew/contact.html')
